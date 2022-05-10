f5d31b4977729e9f86300cf4bf0875448836d4b0
Sahil Makhijani <svmakhijani94@gmail.com>
2022-05-10 01:13:09 -0700

    Added default display text

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 3000;
 
+  app.get('/', (req, res) => {
+    res.send('Hello world!');
+  });
+
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
