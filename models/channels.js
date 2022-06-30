3374b6d3fd4f357404c296c158d20dbc38343408
Sahil Makhijani <svmakhijani94@gmail.com>
2022-06-29 19:29:59 -0700

    Created placeholders for other operations

--- models/channels.js
+++ models/channels.js
@@ -1,10 +1,16 @@
 const { db } = require('../firebase/admin');
 
+const deleteChannel = (req, res) => {};
+
 const getChannels = (res) => {
     const channelsRef = db.ref('/channels');
     channelsRef.on('value', (snapshot => {
         res.send(snapshot.val());
     }));
 }
+
+const postChannel = (req, res) => {};
+
+const putChannel = (req, res) => {};
 
 exports.getChannels = getChannels;
