94c5af3942b6d265b35ccfa41575be5b7cf2420d
Sahil Makhijani <svmakhijani94@gmail.com>
2023-01-15 18:18:56 -0700

    Added user refresh token for Twitch API access

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import getChannels from './models/channels.js';
+import { getToken, updateToken } from './models/token.js';
 import express from 'express';
 
 dotenv.config();
@@ -13,6 +14,22 @@ app.use(bodyParser.json());
 app.get('/channels', async (req, res) => {
   if (isAuthorized(req)) {
     getChannels(res);
+  } else {
+    res.send('Not authorized');
+  }
+});
+
+app.get('/token', async (req, res) => {
+  if (isAuthorized(req)) {
+    getToken(res);
+  } else {
+    res.send('Not authorized');
+  }
+});
+
+app.post('/token', async (req, res) => {
+  if (isAuthorized(req)) {
+    updateToken(req, res);
   } else {
     res.send('Not authorized');
   }
